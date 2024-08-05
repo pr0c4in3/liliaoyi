5f4bd85117c7a4717d2e5d8f740c399b1407d348
procaine <2647788633@qq.com>
2024-08-05 13:03:07 +0800

    1

--- miniprogram/pages/user-center/index.js
+++ miniprogram/pages/user-center/index.js
@@ -10,27 +10,19 @@ Page({
       success: (res) => {
         if (res.code) {
           // 发起网络请求
-          wx.request({
-            url: 'https://your-backend.com/login', // 替换为你的后端登录接口
-            method: 'POST',
-            data: {
-              code: res.code
+          const res = await wx.cloud.callContainer({
+            config: {
+              env: 'prod-6gqmrfeze3773805', // 微信云托管的环境ID
             },
-            success: (res) => {
-              if (res.data.success) {
-                // 假设后端返回用户信息
-                this.setData({
-                  isLogin: true,
-                  userInfo: res.data.userInfo
-                });
-              } else {
-                wx.showToast({
-                  title: '登录失败',
-                  icon: 'none'
-                });
-              }
+            path: '/login', // 填入业务自定义路径和参数，根目录，就是 / 
+            method: 'POST', // 按照自己的业务开发，选择对应的方法
+            header: {
+              'X-WX-SERVICE': 'liliaoyi-backward', // xxx中填入服务名称（微信云托管 - 服务管理 - 服务列表 - 服务名称），在上述实践中是 demo
             }
+            // 其余参数同 wx.request
           });
+          
+          console.log(res);
         } else {
           console.log('获取用户登录态失败！' + res.errMsg);
         }
