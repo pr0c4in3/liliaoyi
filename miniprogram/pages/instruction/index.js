0c8fd8188c5f5010bb2d808825cbb2d7a4daa81f
procaine <2647788633@qq.com>
2024-08-12 12:26:40 +0800

    更新了标题的修改和视频源

--- miniprogram/pages/instruction/index.js
+++ miniprogram/pages/instruction/index.js
@@ -1,66 +1,35 @@
-// pages/instruction/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    introductions: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad: function() {
+    this.fetchIntroductions();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  fetchIntroductions: function() {
+    const that = this;
+    const app = getApp();
+    const baseURL = app.globalData.baseURL; // 获取 baseURL
+    wx.request({
+      url: baseURL+'introductions', // 替换为你的后端API地址
+      method: 'GET',
+      success: function(res) {
+        if (res.statusCode === 200) {
+          that.setData({
+            introductions: res.data.map(item => {
+              return {
+                title: item.title,
+                content: item.content
+              };
+            })
+          });
+        }
+      },
+      fail: function() {
+        wx.showToast({
+          title: '加载失败',
+          icon: 'none'
+        });
+      }
+    });
   }
-})
+});
